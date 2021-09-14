d34a408e57e1b7bef0b8e925f71e33d1441f8751
Thomas <josemich0511@gmail.com>
2021-09-14 12:03:00 +0800

    include bootstap & update cors

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
+  <link rel="stylesheet" href="./css/bootstrap.css">
   <link rel="stylesheet" href="./css/style.css">
 </head>
 
-<body>
+<body class="bg-dark text-light">
   <div id="app">
-    <div class="left">
-      <h1>SMS 發送</h1>
-      <div class="row">
+    <div class="container d-flex">
+      <div class="left flex-column">
+        <h1 class="col">SMS 發送</h1>
         <div class="col">
           帳號：<input type="text" v-model="sendModel.UID" disabled />
         </div>
@@ -21,17 +22,15 @@
           密碼：<input type="password" v-model="sendModel.PWD" disabled />
         </div>
         <div class="col">
-          門號： <input type="text" v-model="sendModel.DEST" />
+          門號：<input type="text" v-model="sendModel.DEST" />
         </div>
         <div class="col">訊息內容：</div>
-        <textarea v-model="sendModel.MSG" cols="30" rows="10"></textarea>
+        <textarea class="col" v-model="sendModel.MSG" cols="30" rows="10"></textarea>
+        <input class="btn btn-warning col" type="button" value="送出" @click="sendSms" />
       </div>
-      <input type="button" value="送出" @click="sendSms" />
-    </div>
 
-    <div class="right">
-      <h1>SMS 回覆</h1>
-      <div class="row">
+      <div class="right flex-column">
+        <h1 class="col">SMS 回覆</h1>
         <div class="col">
           剩餘點數：<input type="text" v-model="sendModel.RES.CREDIT" disabled />
         </div>
@@ -39,17 +38,18 @@
           扣除點數：<input type="text" v-model="sendModel.RES.COST" disabled />
         </div>
         <div class="col">
-          查詢序號： <input type="text" v-model="sendModel.RES.BATCH_ID" disabled />
+          查詢序號：<input type="text" v-model="sendModel.RES.BATCH_ID" disabled />
         </div>
         <div class="col">回覆內容：</div>
-        <textarea v-model="replyModel.RES" cols="30" rows="10"></textarea>
+        <textarea class="col" v-model="replyModel.RES" cols="30" rows="10"></textarea>
       </div>
-      <h1>
     </div>
+
   </div>
 
   <script src="./js/vue.global.prod.js"></script>
   <script src="./js/axios.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
   <script src="./js/main.js" type="module"></script>
 </body>
 
