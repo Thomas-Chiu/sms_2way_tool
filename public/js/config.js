2415f48e916edfea4a4c0d005dcf97f136b36e10
Thomas <josemich0511@gmail.com>
2021-09-15 15:06:12 +0800

    fix const

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,10 @@
-const isDev = false;
 const username = "0952260525";
 const password = "Jasslin13091876";
 const taxId = "13091876";
-const host = "";
 const corsPort = 4000;
 const webPort = 3000;
+let isDev = false;
+let host = "";
 
 isDev = false ? (host = "60.251.157.49") : (host = "localhost");
 
